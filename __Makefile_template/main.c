17f5e5e52203d90ba0e3d36e36a7f43ac32524b6
louielouie <louie.desfontaines@gmail.com>
2021-07-06 00:01:50 +0200

    makefile template wip - to cleeean

--- __Makefile_template/main.c
+++ __Makefile_template/main.c
@@ -6,11 +6,12 @@
 /*   By: louielouie <louielouie@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 11:41:19 by louielouie        #+#    #+#             */
-/*   Updated: 2021/07/05 11:48:03 by louielouie       ###   ########.fr       */
+/*   Updated: 2021/07/05 22:32:56 by louielouie       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "includes/main.h"
 
 int	main(int ac, char **av)
 {
