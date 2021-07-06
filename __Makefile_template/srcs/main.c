4a6fa9dad0f29e86de2f6be9906c8fdeff80698e
louielouie <louie.desfontaines@gmail.com>
2021-07-06 13:17:43 +0200

    makefile ok

--- __Makefile_template/srcs/main.c
+++ __Makefile_template/srcs/main.c
@@ -6,11 +6,10 @@
 /*   By: louielouie <louielouie@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 11:41:19 by louielouie        #+#    #+#             */
-/*   Updated: 2021/07/06 12:49:33 by louielouie       ###   ########.fr       */
+/*   Updated: 2021/07/06 13:17:38 by louielouie       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "../includes/main.h"
 
 int	main(int ac, char **av)
