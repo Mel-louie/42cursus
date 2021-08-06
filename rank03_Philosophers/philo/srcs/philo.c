8544525936f3fede5289942cf2533fb975a7f63d
mel-louie <louie.desfontaines@gmail.com>
2021-08-06 16:18:22 +0200

    update philo

--- rank03_Philosophers/philo/srcs/philo.c
+++ rank03_Philosophers/philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: mdesfont <mdesfont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 11:41:19 by louielouie        #+#    #+#             */
-/*   Updated: 2021/08/06 15:30:23 by mdesfont         ###   ########.fr       */
+/*   Updated: 2021/08/06 15:56:24 by mdesfont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,32 +36,56 @@ int	main(int ac, char **av)
 	//
 	int j = 0;
 	int tmp;
-	if (s.ps->must_eat_nb != -1)
-		tmp = s.ps->must_eat_nb;
-	else
-		tmp = 1; //suppr to push, doit etre infini
-	//printf("%d\n", tmp);
-	while(j < tmp)
+	tmp = s.ps->must_eat_nb;
+	if (tmp != -1)
 	{
-	//	printf("%d\n", j);
-		pthread_mutex_lock(s.ps->blockmoni);
-		curr = str;
-		total = 0;
-		for(i = 0; i < s.ps->philos_nb; i++)
-			total += s.ps->blocknum[i];
-		sprintf(curr, "%3ld Total blocktime: %5d : ",
-			time(0) - s.ps->t0, total);
-		curr = str + strlen(str);
-		for(i=0; i < s.ps->philos_nb; i++)
+		while(j < tmp)
 		{
-			sprintf(curr, "%5d ", s.ps->blocknum[i]);
+		//	printf("%d\n", j);
+			pthread_mutex_lock(s.ps->blockmoni);
+			curr = str;
+			total = 0;
+			for(i = 0; i < s.ps->philos_nb; i++)
+				total += s.ps->blocknum[i];
+			sprintf(curr, "%3ld Total blocktime: %5d : ",
+			time(0) - s.ps->t0, total);
 			curr = str + strlen(str);
+			for(i=0; i < s.ps->philos_nb; i++)
+			{
+				sprintf(curr, "%5d ", s.ps->blocknum[i]);
+				curr = str + strlen(str);
+			}
+			pthread_mutex_unlock(s.ps->blockmoni);
+			printf("%s\n", str);
+			fflush(stdout);
+			sleep(10);
+			j++;
 		}
-		pthread_mutex_unlock(s.ps->blockmoni);
-		printf("%s\n", str);
-		fflush(stdout);
-		sleep(10);
-		j++;
+	}
+	else
+	{
+		while(1)
+		{
+		//	printf("%d\n", j);
+			pthread_mutex_lock(s.ps->blockmoni);
+			curr = str;
+			total = 0;
+			for(i = 0; i < s.ps->philos_nb; i++)
+				total += s.ps->blocknum[i];
+			sprintf(curr, "%3ld Total blocktime: %5d : ",
+			time(0) - s.ps->t0, total);
+			curr = str + strlen(str);
+			for(i=0; i < s.ps->philos_nb; i++)
+			{
+				sprintf(curr, "%5d ", s.ps->blocknum[i]);
+				curr = str + strlen(str);
+			}
+			pthread_mutex_unlock(s.ps->blockmoni);
+			printf("%s\n", str);
+			fflush(stdout);
+			sleep(10);
+			j++;
+		}
 	}
 	//
 	
