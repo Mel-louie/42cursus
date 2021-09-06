f1e6b6dbc10fbf1592fbb7a919a6179e45a2d3d4
mel-louie <louie.desfontaines@gmail.com>
2021-09-06 15:00:55 +0200

    progress

---
+++ rank03_Philosophers/newPhilo/tester.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+#    COLORS    #
+
+_BLACK="\033[30m"
+_RED="\033[31m"
+_GREEN="\033[32m"
+_YELLOW="\033[33m"
+_BLUE="\033[34m"
+_VIOLET="\033[35m"
+_CYAN="\033[36m"
+_WHITE="\033[37m"
+_END="\033[0m"
+# Bold
+_BBLACK="\033[1;30m"		# BLACK
+_BRED="\033[1;31m"			# RED
+_BGREEN="\033[1;32m"		# GREEN
+_BYELLOW="\033[1;33m"		# YELLOW
+_BBLUE="\033[1;34m"			# BLUE
+_BPURPLE="\033[1;35m"		# PURPLE
+_BCYAN="\033[1;36m"			# CYAN
+_BWHITE="\033[1;37m"		# WHITE
+
+FILE="errors_log"
+
+make re
+printf "${_BGREEN}\n(•_•)\t( •_•)>⌐■-■\t(⌐■_■)\n${_END}"
+printf "${_BYELLOW}Philosophers successfully compiled\n\n${_END}"
+printf "░█▀▀█ ░█─░█ ▀▀█▀▀ ░█     ▄▀▀▀▄   ▀▀█▀▀ ░█▀▀▀ ░█▀▀▀█ ▀▀█▀▀ ░█▀▀▀ ░█▀▀█ \n"
+printf "░█▄▄█ ░█▀▀█ ─░█── ░█─── ░█  ░█── ─░█── ░█▀▀▀ ─▀▀▀▄▄ ─░█── ░█▀▀▀ ░█▄▄▀ \n"
+printf "░█─── ░█─░█ ▄▄█▄▄ ░█▄▄▄  ▀▄▄▄▀   ─░█── ░█▄▄▄ ░█▄▄▄█ ─░█── ░█▄▄▄ ░█─░█ \n"
+printf "\n\t\t${_BCYAN}Welcome to my 42 Philosophers tester.${_END}\n\n"
+
+printf "Usage: copy this file in your project's folder, then:"
+printf "\n\t${_GREEN}~ > bash tester.sh${_END}\n\n"
+printf "In Case of a failed test, please check ./errors_log file\n\n"
+
+if [ -f "$FILE" ]; then
+	rm "$FILE"
+fi
+
+error_log ()
+{
+	echo "[$1-$2]: $3" >> ./errors_log
+}
+
+test_one ()
+{
+	(./philo 4 310 200 100 > "./log_$1")&
+	sleep 5
+	pkill $1
+	output=$(grep died -m 1 "./log_$1" | awk '{print $NF}')
+	if [ $output = "died" ];then
+		printf "${_GREEN} Test #1 Succeeded!${_END}\n"
+	else
+		printf "${_RED} Test #1 Failed!${_END}\n"
+		error_log $1 "Test #1" "Given 4 310 200 100 arguments to $1, a philosopher should die !"
+	fi
+	rm -rf "./log_$1"
+}
+
+test_two ()
+{
+	printf "${_CYAN} Test #2: Executing your program for 180 second, please wait...${_END}\n"
+	(./philo 4 410 200 200 > /dev/null)&
+	i=1
+	error=0
+	while [ $i -lt 180 ]; do
+		printf "\r[%d...]" $i
+		pgrep $1 > /dev/null
+		if [ "$?" -ne 0 ]; then
+		printf "\r${_RED} Test #2 Failed!${_END}\n"
+			error_log $1 "Test #2" "Given 4 410 200 200 arguments to $1, no philosopher should die! - is dead at $i second"
+			error=1
+			break
+		fi
+		sleep 1
+		i=$(( $i + 1 ))
+	done
+	sleep 1
+	if [ $error -eq 0 ]; then
+		pkill $1
+		printf "${_GREEN} Test #2 Succeeded!${_END}\n"
+	fi
+}
+
+test_three ()
+{
+	printf "${_CYAN} Test #3: Executing your program for 180 second, please wait...${_END}\n"
+	(./philo 5 800 200 200 > /dev/null)&
+	i=1
+	error=0
+	while [ $i -lt 180 ];do
+		printf "\r[%d...]" $i
+		pgrep $1 > /dev/null
+		if [ "$?" -ne 0 ];then
+			printf "\r${_RED} Test #3 Failed!${_END}\n"
+			error_log $1 "Test #3" "Given 4 800 200 200 arguments to $1, no philosopher should die! - is dead at $i second"
+			error=1
+			break
+		fi
+		sleep 1
+		i=$(( $i + 1 ))
+	done
+	sleep 1
+	if [ $error -eq 0 ];then
+		pkill $1
+		printf "${_GREEN} Test #3 Succeeded!${_END}\n"
+	fi
+}
+
+test_four ()
+{
+	(./philo 4 410 200 200 $3 > "./log_$1")&
+	sleep 10
+	pgrep $1 > /dev/null
+	if [ "$?" -eq 1 ];then
+		lines=$(grep eating "./log_$1" | wc -l)
+		if [ $lines -ge $4 ];then
+			echo "\t\e[92m[+] Test #4-$5 Succeeded\e[0m"
+		else
+			echo "\t\e[91m[+] Test #4-$5 Failed\e[0m"
+			error_log $1 "Test #4" "Given 4 410 200 200 $3 arguments to $1, $1 should only be stopped if each philosopher ate at least $3 times !"
+		fi
+	else
+		echo "\t\e[91m[+] Test #4-$5 Failed\e[0m"
+		error_log $1 "Test #4" "Given 4 410 200 200 $3 arguments to $1, $1 should stop !"
+		pkill $1
+	fi
+	rm -rf "./log_$1"
+}
+
+test_five ()
+{
+	echo "\e[94m[+] Test #5 on progress, please wait...\e[0m"
+	i=1
+	t=0
+	error=0
+	while [ $i -le 10 ];do
+		(./philo 2 60 60 60 > "./log_$1")&
+		sleep 2
+		pgrep $1 > /dev/null
+		if [ "$?" -eq 1 ];then
+			printf "\r[%d/10]" $i
+			tmp=$(grep died -m 1 "./log_$1" | awk '{print $1}' | sed 's/[^0-9]*//g')
+			if [ $i -gt 1 ];then
+				x=$(expr $tmp - $t)
+				x=${x#-}
+				if [ $x -gt 10 ];then
+					printf "\r\e[91m[+] Test #5 Failed\e[0m\n"
+					error_log $1 "Test #5" "Given 2 60 60 60 arguments to $1, the time difference of each death shouldn't be bigger than 10ms !"
+					error=1
+					break
+				fi
+			else
+				t=$tmp
+			fi
+		else
+			printf "\r\e[91m[+] Test #5 Failed\e[0m\n"
+			error_log $1 "Test #5" "Given 2 60 60 60 arguments to $1, a philosopher should die !"
+			pkill $1
+			break
+		fi
+		i=$(( $i + 1 ))
+	done
+
+	if [ $error -eq 0 ];then
+		echo "\r\e[92m[+] Test #5 Succeeded\e[0m"
+	fi
+}
+
+tests ()
+{
+	echo "\n[============[ Testing philo ]==============]\n"
+
+	target="philo"
+
+	test_one $target $1
+
+	test_two $target $1
+
+	test_three $target $1
+
+	printf "${_CYAN}Tests #4 in progress, please wait...${_END}\n"
+	test_four $target $1 7 28 1
+	test_four $target $1 10 40 2
+	test_four $target $1 12 48 3
+	test_four $target $1 15 60 4
+
+	test_five $target $1
+	rm -rf "./log_$target"
+}
+
+tests
--- rank03_Philosophers/toDo.md
+++ rank03_Philosophers/toDo.md
@@ -1,8 +1,9 @@
 🔘 commenter et documenter 
-🔘 tester valgrind leaks (linux)
-🔘 sortir de la simu en cas de ./philo 1 200 200 200 (au bout de 200ms)
+🔘 probleme quand philo dies apres que tous est pris une fork (ne sort pas du prog) > voir test_one
 🔘 check si mes explications sont bonnes (cheat sheet bullet points dans le cahier)
+✅ faire un mini tester
 <br />
+✅ tester valgrind leaks (linux)
 ✅ parsing
 ✅ vérif le nb max de philos en correction
 ✅ add time to die et number must eat
