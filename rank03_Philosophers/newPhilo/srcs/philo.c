64a735195e1856a5982240b399cf87ff7533114b
mel-louie <louie.desfontaines@gmail.com>
2021-09-06 18:31:54 +0200

    update philo

--- rank03_Philosophers/newPhilo/srcs/philo.c
+++ rank03_Philosophers/newPhilo/srcs/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mdesfont <mdesfont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 16:18:21 by mdesfont          #+#    #+#             */
-/*   Updated: 2021/09/06 16:56:56 by user42           ###   ########.fr       */
+/*   Updated: 2021/09/06 18:30:31 by mdesfont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,53 @@ void	one_philo(t_philo *philo, t_data *data)
 		pthread_mutex_unlock(philo->lfork_mutex);
 }
 
+int	check_num(char *str)
+{
+	int	c;
+
+	c = 0;
+	while (str[c])
+	{
+		if (str[c] < '0' || str[c] > '9')
+			return (1);
+		c++;
+	}
+	return (0);
+}
+
+int	handle_errors(int ac, char **av)
+{
+	int	i;
+
+	i = ac - 1;
+	while (i)
+	{
+		if (check_num(av[i]) == 1)
+		{
+			printf("Error\nYou can only run the program with");
+			printf(" positives integers.\n");
+			return (1);
+		}
+		i--;
+	}
+	if (ac < 5 || ac > 6)
+	{
+		printf("Wrong number of arguments.\n");
+		return (1);
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	t_data		data;
 	t_philo		*philo;
 	pthread_t	*thread;
 
-	if (ac < 5 || ac > 6)
-		return (ft_putstr_fd("Wrong number of arguments\n", 1, 0));
+	if (handle_errors(ac, av) == 1)
+		return (1);
 	if (parsing(&data, ac, av))
-		return (ft_putstr_fd("Parsing error\n", 1, 0));
+		return (ft_putstr_fd("Error\nWrong type of arguments\n", 1, 0));
 	philo = malloc(sizeof(t_philo) * data.num);
 	if (!philo)
 		return (ft_putstr_fd("Can't malloc\n", 1, 0));
@@ -35,7 +72,7 @@ int	main(int ac, char **av)
 	if (!thread)
 		return (ft_putstr_fd("Can't malloc\n", 1, 0));
 	if (set_threads(&data, philo, thread))
-		return (ft_putstr_fd("Thread error\n", 1, 0));
+		return (ft_putstr_fd("Something went wrong with threads init\n", 1, 0));
 	free(philo);
 	free(thread);
 	free(data.forks);
--- rank03_Philosophers/toDo.md
+++ rank03_Philosophers/toDo.md
@@ -1,11 +1,11 @@
 🔘 check si mes explications sont bonnes (cheat sheet bullet points dans le cahier)
 🔘 reviser (onglets mac school)
 🔘 tester sur mac os
+🔘 tester tous les tests de la cheatsheet
 <br />
 ✅ normer
 ✅ probleme quand philo dies apres que tous est pris une fork (ne sort pas du prog) > voir test_one
 ✅ commenter et documenter 
-✅ faire un mini tester
 ✅ tester valgrind leaks (linux)
 ✅ parsing
 ✅ vérif le nb max de philos en correction
@@ -13,4 +13,22 @@
 ✅  nettoyer blocknum/moni
 ✅  nettoyer fflush
 ✅  print les changements de status
-✅  ft_exit
+
+
+cheatsheet
+
+test 	resultat attendu 
+./philo 1 200 200 200 	philo 1 ne prend qu'une fourchette et meurt au bout de 200 ms
+./philo 1 800 200 200
+./philo 2 800 200 200 	personne ne meurt
+./philo 5 800 200 200 	personne ne meurt
+./philo 5 800 200 200 7 	la simulation s'arrete quand chaque philo a mange 7 fois
+./philo 4 410 200 200 	personne ne meurt
+./philo 4 310 200 200 	un philo meurt
+./philo 4 500 200 1.2 	argument invalide
+./philo 4 0 200 200 	argument invalide
+./philo 4 -500 200 200 	argument invalide
+./philo 4 500 200 2147483647 	un philo meurt au bout de 500 ms
+./philo 4 2147483647 200 200 	personne ne meurt
+./philo 4 214748364732 200 200 	argument invalide
+./philo 4 200 210 200 	un philo meurt, il faut afficher la mort avant 210 ms
