f01b1a42e236f965fea9b188b117b39aefa4df7e
louielouie <louie.desfontaines@gmail.com>
2021-09-10 13:47:38 +0200

    wip

--- rank04_CPP/CPP_00/ex00/megaphone.cpp
+++ rank04_CPP/CPP_00/ex00/megaphone.cpp
@@ -6,14 +6,34 @@
 /*   By: louielouie <louielouie@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 12:01:56 by louielouie        #+#    #+#             */
-/*   Updated: 2021/09/10 12:09:06 by louielouie       ###   ########.fr       */
+/*   Updated: 2021/09/10 13:47:02 by louielouie       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+void	megaphone(int ac, char **av)
 {
-	std::cout << "Hello world!\n";
+	int		i = 0;
+	int		j = 0;
+
+	while (++i < ac)
+	{
+		while (av[i][j])
+		{
+			putchar(toupper(av[i][j]));
+			j++;
+		}
+		j = 0;
+	}
+	std::cout << std::endl;
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+		megaphone(ac, av);
 	return (0);
 }
