372fc87194ca7874f82670e9d561aea7e11539f6
louielouie <louie.desfontaines@gmail.com>
2021-09-16 20:06:19 +0200

    exam04 skeleton

--- rank04_exam_microshell/microshell/main.c
+++ rank04_exam_microshell/microshell/main.c
@@ -26,9 +26,20 @@ int	size_cmd(char **cmd, int i, t_shell *sh)
 	return (sh->end);
 }
 
+int exit_fatal(char **ptr)
+{
+	if (ptr != NULL)
+		free(ptr);
+	write(2, "error: fatal\n", ft_strlen("error: fatal\n"));
+	exit(EXIT_FAILURE);
+	return (EXIT_FAILURE);
+}
+
 char	*add_cmd(char **cmd, int i, t_shell *sh)
 {
 	char	*tmp = malloc(sizeof(char) * ft_strlen(cmd[i]));
+	if (!tmp)
+		exit_fatal(NULL);
 	sh->end = 0;
 	sh->start = 0;
 
@@ -49,7 +60,7 @@ int	built_cd(char *cd, int i, char **cmd)
 	int i_path = i + 1;
 	// printf("cmd i %s +1 %s", cmd[i], cmd[i + 1]);
 	if (!cmd[i_path] || !strncmp(cmd[i_path], "-", 1))
-		write(2, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n"));
+		return(write(2, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n")));
 	if (chdir(cmd[i_path]) != 0)
 	{
 		write(2, "error: cd: cannot change directory to ", ft_strlen("error: cd: cannot change directory to "));
@@ -59,13 +70,52 @@ int	built_cd(char *cd, int i, char **cmd)
 	return (0);
 }
 
-int main(int ac, char **av, char **env)
+int	execute(char **cmd, char *arg, char **envp)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(arg, cmd, envp);
+		exit(0);
+	}
+	else if (pid < 0)
+		exit_fatal(cmd);
+	waitpid(0, NULL, 0);
+	return (1);
+}
+
+
+int	find_pipe(char **cmd)
+{
+	if (!cmd)
+		return (NULL);
+	int i = -1;
+	while (cmd[++i])
+		if (!strcmp(cmd[i], "|"))
+			return (&cmd[i + 1]);
+	return (NULL);
+}
+
+int	ft_execute(char **cmd, char *arg, char **envp)
+{
+	//no pipe
+	if (!find_pipe(cmd))
+		execute(cmd, arg, envp);
+	else
+		exe_pipe(cmd, arg, envp);
+		printf("pipe\n");
+	return (0);
+}
+
+int main(int ac, char **av, char **envp)
 {
 	t_shell	sh;
 	int	i = 1;
 	char	*cmd = NULL;
+	char	**args = av + 1;
 
-	sh.env = env;
 	if (ac < 2)
 		return (0);
 	while (i < ac)
@@ -74,7 +124,8 @@ int main(int ac, char **av, char **env)
 		{
 			if (!strcmp(cmd, "cd"))
 				built_cd(cmd, i, av);
-			//  printf("cmd %s\n", cmd);
+			else
+			 	ft_execute(args, cmd, envp);
 		}
 		free(cmd);
 		cmd = NULL;
