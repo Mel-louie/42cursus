81b92e0fad495f3f7b5408d32b7afeeec81ceb40
mel-louie <louie.desfontaines@gmail.com>
2021-10-08 14:30:07 +0200

    update phonebook

--- rank04_CPP/CPP_00/ex01/Class.phonebook.cpp
+++ rank04_CPP/CPP_00/ex01/Class.phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: mdesfont <mdesfont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 14:05:08 by mdesfont          #+#    #+#             */
-/*   Updated: 2021/10/08 14:28:10 by mdesfont         ###   ########.fr       */
+/*   Updated: 2021/10/08 14:30:03 by mdesfont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,9 +49,33 @@ void	Phonebook::table_of_contents(void)
 			else if (choice == "EXIT")
 				exit(0);
 			else if (choice == "ADD")
-				this->_addContact();
+				this->add();
 			else if (choice == "SEARCH")
-				this->_searchContact();
+				this->search();
 		}
 }
 
+void	Phonebook::search(void) {
+	if (!this->_nbContact) {
+		std::cout << "No contacts in phoneBook" << std::endl;
+		return ;	
+	}
+	std::cout << "|   INDEX  |FIRST NAME| LAST NAME| NICKNAME |" << std::endl;
+	for (int i = 0 ; i < _nbContact ; i++) {
+		std::cout << "|" << std::right << std::setw(10) << i;
+		this->_contacts[i].printContact();
+	}
+	int	index;
+	std::cout << "Type in contact index: ";
+	std::cin >> index;
+	if (std::cin.fail() || index >= this->_nbContact || index < 0) {
+		std::cout << "Please type in an integer between 0 and " << this->_nbContact - 1 << std::endl;
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+	}
+	else {
+		this->_contacts[index].printFullContact();
+		std::cin.ignore(INT_MAX, '\n');
+	}
+	return ;
+}
