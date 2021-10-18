fe4df7483cb6d574fce5179fa0840d0c10f91e55
mel-louie <louie.desfontaines@gmail.com>
2021-10-18 17:48:45 +0200

    buffer is catch

--- rank04_CPP/CPP_01/ex04/main.cpp
+++ rank04_CPP/CPP_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:09:14 by user42            #+#    #+#             */
-/*   Updated: 2021/10/18 17:42:52 by user42           ###   ########.fr       */
+/*   Updated: 2021/10/18 17:47:28 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,63 +16,21 @@
 #include <sstream>
 #include "replace.hpp"
 
-void	displayUsages()
-{
-	std::cout << "The program takes a filename and two strings, which are NOT empty." 
-	<< std::endl;
-	std::cout << "Like this:" << std::endl
-	<< "\t./replace [filename] [string1] [string2]" << std::endl;
-}
-
-int	checkArgs(int ac, char **av, std::string replaceFilename)
-{
-	int	ret = 0;
-
-	if (ac != 4 || av[1][0] == '\0' || av[2][0] == '\0' || av[3][0] == '\0')
-	{
-
-		displayUsages();
-		return ( ret = 1 );
-	}
-
-	replaceFilename = av[1];
-	replaceFilename.append(".replace");
-	std::cout << replaceFilename << std::endl;
-
-	return (ret);
-}
-
-std::string	getBuffer(std::string fileName)
-{
-	std::string	dest;
-	std::ostringstream	buffer;
-	std::ifstream	file( fileName );
-
-	if ( file )
-	{
-		buffer << file.rdbuf();
-		file.close();
-
-		dest = buffer.str();
-		
-	}
-	
-	return ( dest );
-}
-
 int	main(int ac, char **av)
 {
 	std::string	replaceFilename = "";
 	if (checkArgs( ac, av, replaceFilename ))
 		return (1);
 
+//	std::cout << replaceFilename;
+
 	std::string	content;
 	std::string	fileName = av[1];
 	content = getBuffer(fileName);
 	
-	std::cout << "bufbeg:\n" << content << "bufend." << std::endl;
+//	std::cout << "bufbeg:\n" << content << "bufend." << std::endl;
 	
-	//fonction pour replace
+	
 
 	return (0);
 }
--- rank04_CPP/CPP_01/ex04/replace.hpp
+++ rank04_CPP/CPP_01/ex04/replace.hpp
@@ -6,13 +6,15 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 17:29:41 by user42            #+#    #+#             */
-/*   Updated: 2021/10/18 17:30:03 by user42           ###   ########.fr       */
+/*   Updated: 2021/10/18 17:44:56 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REPLACE_HPP
 # define REPLACE_HPP
 
-
+void	displayUsages();
+int	checkArgs(int ac, char **av, std::string replaceFilename);
+std::string	getBuffer(std::string fileName);
 
 #endif
