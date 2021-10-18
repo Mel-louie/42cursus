f3f0f309c39a505ab7b23881a8a598675695ca07
mel-louie <louie.desfontaines@gmail.com>
2021-10-18 16:54:09 +0200

    ok weapon

--- rank04_CPP/CPP_01/ex04/main.cpp
+++ rank04_CPP/CPP_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:09:14 by user42            #+#    #+#             */
-/*   Updated: 2021/10/18 16:52:14 by user42           ###   ########.fr       */
+/*   Updated: 2021/10/18 16:54:03 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ int	main(int ac, char **av)
 	std::ifstream	file( av[1] );
 	std::string	dest;
 	
+
+	//faire un check pour checker les argu
+
+	//fonction pour recup dest
+	
 	if ( file )
 	{
 		std::ostringstream	buffer;
@@ -30,5 +35,7 @@ int	main(int ac, char **av)
 		dest = buffer.str();
 		std::cout << dest << std::endl;
 	}
+
+	//fonction pour replace
 	return (0);
 }
