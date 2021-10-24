7df54da3df0f7d28f252ccd4cd77f538d917c5e8
louielouie <louie.desfontaines@gmail.com>
2021-10-24 18:43:27 +0200

    cpp02 ok to retry

--- rank04_CPP/CPP_02/ex02/main.cpp
+++ rank04_CPP/CPP_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: louielouie <louielouie@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 12:31:21 by louielouie        #+#    #+#             */
-/*   Updated: 2021/10/24 18:39:16 by louielouie       ###   ########.fr       */
+/*   Updated: 2021/10/24 18:43:08 by louielouie       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main(void) {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
 
+//increment decrement
 	std::cout << "a\t= " << a << std::endl;
 	std::cout << "++a\t= " << ++a << std::endl;
 	std::cout << "a\t= " << a << std::endl;
@@ -33,21 +34,24 @@ int main(void) {
 
 	std::cout << "b\t= " << b << std::endl;
 	std::cout << std::endl;
-	
-	std::cout << "a < b ?\t= " << a.operator<(b) << std::endl;
-	
-	std::cout << "a > b ?\t= " << a.operator>(b) << std::endl;
+
+//comparaison operators
+	std::cout << "a < b ?\t\t= " << a.operator<(b) << std::endl;
+	std::cout << "a > b ?\t\t= " << a.operator>(b) << std::endl;
+	std::cout << "a == b ?\t= " << a.operator==(b) << std::endl;
+	std::cout << "a != b ?\t= " << a.operator!=(b) << std::endl;
+	std::cout << "a >= b ?\t= " << a.operator>=(b) << std::endl;
+	std::cout << "a <= b ?\t= " << a.operator<=(b) << std::endl;
 	std::cout << std::endl;
 
+//arithmetic operators
 	std::cout << "b / a\t= " << b.operator/(a) << std::endl;
-
 	std::cout << "b * a\t= " << b.operator*(a) << std::endl;
-
 	std::cout << "b + a\t= " << b.operator+(a) << std::endl;
-
 	std::cout << "b - a\t= " << b.operator-(a) << std::endl;
 	std::cout << std::endl;
 
+//max min
 	std::cout << "max(" << a << ", " << b << ")\t = " << Fixed::max(a, b) << std::endl;
 	std::cout << "min(" << a << ", " << b << ")\t = " << Fixed::min(a, b) << std::endl;
 	std::cout << std::endl;
