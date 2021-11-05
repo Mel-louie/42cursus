3af94f449dc9aad3e375614097e3df9e4822854a
louielouie <louie.desfontaines@gmail.com>
2021-11-05 15:48:10 +0100

    wip 08

--- rank04_CPP/CPP_08/ex01/includes/span.hpp
+++ rank04_CPP/CPP_08/ex01/includes/span.hpp
@@ -6,12 +6,60 @@
 /*   By: louielouie <louielouie@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 12:30:36 by louielouie        #+#    #+#             */
-/*   Updated: 2021/11/05 15:23:28 by louielouie       ###   ########.fr       */
+/*   Updated: 2021/11/05 15:43:32 by louielouie       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <algorithm>
+# include <vector>
+
+class Span
+{
+private:
+	std::vector<int> _nbs;
+	size_t _size_max;
+
+	class object_full: public std::exception {
+
+	public:
+	
+		const char *what() const throw();
+	
+	};
+	class no_span: public std::exception {
+
+	public:
+	
+		const char *what() const throw();
+	
+	};
+
+	Span();
+
+public:
+	Span( unsigned int N );
+	Span( const Span& cpy );
+	~Span( void );
+
+	Span&	operator=( const Span& src );
+
+	void	addNumber( int nb );
+	template<typename T>
+	void addNumber( T start, T end ) {		
+
+		for ( T it = start; it < end; it++ ) {
+			if ( this->_nbs.size() < _size_max )
+				_nbs.push_back( *it );
+			else
+				throw (Span::object_full());
+		}
+		return ;
+	}
+	unsigned int shortestSpan( void );
+	unsigned int longestSpan( void );
+};
 
 #endif
