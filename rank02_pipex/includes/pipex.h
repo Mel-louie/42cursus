7894826dedf92e9e26c675e51af52d74460ff3a0
louielouie <louie.desfontaines@gmail.com>
2021-11-10 17:19:21 +0100

    pipex

---
+++ rank02_pipex/includes/pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: louielouie <louielouie@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 17:00:21 by louielouie        #+#    #+#             */
+/*   Updated: 2021/11/10 17:18:55 by louielouie       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <unistd.h>
+
+void	display_usages(void);
+int		ft_strlen(const char *str);
+
+#endif
