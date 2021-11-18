baefd7a0a7cb1a49c58cc06d608578c6b134eadf
Mel DESFONTAINES <mdesfont@e3r4p23.clusters.42paris.fr>
2021-11-18 13:18:17 +0100

    g froid

--- rank04_exam_microshell/microshell/test.c
+++ rank04_exam_microshell/microshell/test.c
@@ -1,23 +1,8 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <stdio.h>
-
-int error(char *s)
-{
-	int i = -1;
-	while (s[++i])
-		write(STDERR_FILENO, &s[i], 1);
-	return (1);
-}
-
-int fatal(char **ptr)
-{
-	if (ptr != NULL)
-		free(ptr);
-	exit(error("error: fatal\n"));
-}
 
 char **find_pipe(char **cmd)
 {
@@ -26,7 +11,7 @@ char **find_pipe(char **cmd)
 	int i = -1;
 	while (cmd[++i])
 		if (!strcmp(cmd[i], "|"))
-			return (&cmd[i + 1]);
+			return (&cmd[i+1]);
 	return (NULL);
 }
 
@@ -41,17 +26,32 @@ int	size_char(char **cmd, char *str)
 	return (i);
 }
 
-int exe_cmd(char **cmd, char **env)
+int	error(char *s)
+{
+	int i =-1;
+
+	while (s[++i])
+		write(STDERR_FILENO, &s[i], 1);
+	return (1);
+}
+
+int fatal(char **ptr)
+{
+	free(ptr);
+	exit(error("error: fatal\n"));
+}
+
+int	exe_cmd(char **cmd, char **env)
 {
 	pid_t pid;
 
 	if ((pid = fork()) < 0)
-		 fatal(cmd);
+		fatal(cmd);
 	if (!pid)
 	{
 		if (execve(cmd[0], cmd, env) < 0)
 		{
-			error("exec error ");
+			error("exec error: ");
 			error(cmd[0]);
 			free(cmd);
 			exit(error("\n"));
@@ -61,13 +61,13 @@ int exe_cmd(char **cmd, char **env)
 	return (0);
 }
 
-int exe_child(char **cmd, char **env, char **tmp, int *fd_pipe, int fd_in)
+int	exe_child(char **cmd, char **tmp, int *fd_pipe, int fd_in, char **env)
 {
 	if (dup2(fd_in, STDIN_FILENO) < 0)
 		fatal(cmd);
 	if (find_pipe(tmp) && dup2(fd_pipe[1], STDOUT_FILENO) < 0)
 		fatal(cmd);
-	
+
 	close(fd_in);
 	close(fd_pipe[0]);
 	close(fd_pipe[1]);
@@ -75,32 +75,31 @@ int exe_child(char **cmd, char **env, char **tmp, int *fd_pipe, int fd_in)
 	tmp[size_char(tmp, "|")] = NULL;
 	exe_cmd(tmp, env);
 
-	if (cmd)
-		free(cmd);
+	free(cmd);
 	exit(0);
 }
 
-int execute(char **cmd, char **env)
+int	execute(char **cmd, char **env)
 {
 	if (find_pipe(cmd) == NULL)
 		return (exe_cmd(cmd, env));
 
-	int nbwait = 0;
-	char **tmp = cmd;
 	int fd_in;
 	int fd_pipe[2];
+	int nb_wait = 0;
+	char **tmp = cmd;
 	pid_t pid;
 
 	if ((fd_in = dup(STDIN_FILENO)) < 0)
-		return (fatal(cmd));
-
+		fatal(cmd);
+	
 	while (tmp)
 	{
 		if (pipe(fd_pipe) < 0 || (pid = fork()) < 0)
 			fatal(cmd);
-		
+
 		if (!pid)
-			exe_child(cmd, env, tmp, fd_pipe, fd_in);
+			exe_child(cmd, tmp, fd_pipe, fd_in, env);
 
 		else
 		{
@@ -108,17 +107,37 @@ int execute(char **cmd, char **env)
 				fatal(cmd);
 			close(fd_pipe[0]);
 			close(fd_pipe[1]);
-			++nbwait;
+			++nb_wait;
 			tmp = find_pipe(tmp);
 		}
-
 	}
 
 	close(fd_in);
-	
-	while (--nbwait >= 0)
-		waitpid(0,NULL,0);
+
+	while (--nb_wait >= 0)
+		waitpid(0, NULL, 0);
+
 	return (0);
+}
+
+char **add_cmd(char **av, int *i)
+{
+	int size = size_char(&av[*i], ";");
+	if (!size)
+		return (NULL);
+	
+	char **tmp = NULL;
+	if (!(tmp = malloc(sizeof(*tmp) * (size + 1))))
+		fatal(NULL);
+	
+	int j = -1;
+	while (++j < size)
+		tmp[j] = av[j + *i];
+	tmp[j] = NULL;
+
+	*i += size;
+
+	return (tmp);
 }
 
 int size_cmd(char **cmd)
@@ -130,36 +149,19 @@ int size_cmd(char **cmd)
 	return (i);
 }
 
-int	builtin_cd(char **cmd)
+int builtin_cd(char **cmd)
 {
 	if (size_cmd(cmd) != 2)
-		return (error("cd bad arg\n"));
+		return (error("bad args cd\n"));
 	if (chdir(cmd[1]) < 0)
 	{
-		error("cd bad path ");
+		error("cant go to ");
 		error(cmd[1]);
 		error("\n");
 	}
 	return (0);
 }
 
-char **add_cmd(char **av, int *i)
-{
-	int size = size_char(&av[*i], ";");
-	if (!size)
-		return (NULL);
-
-	char **tmp = NULL;
-	if (!(tmp = malloc(sizeof(*tmp) * (size + 1))))
-		fatal(NULL);
-
-	int j = -1;
-	while (++j < size)
-		tmp[j] = av[j + *i];
-	tmp[j] = NULL;
-	*i += size;
-	return (tmp);
-}
 
 int	main(int ac, char **av, char **env)
 {
@@ -172,10 +174,11 @@ int	main(int ac, char **av, char **env)
 
 		if (cmd && !strcmp(cmd[0], "cd"))
 			builtin_cd(cmd);
+
 		else if (cmd)
 			execute(cmd, env);
 
-		//if (cmd)
+		if (cmd)
 			free(cmd);
 		cmd = NULL;
 	}
