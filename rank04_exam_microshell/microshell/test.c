d952754f053fb4b8ee03f1f4390defd60a4f3228
Mel DESFONTAINES <mdesfont@e3r4p23.clusters.42paris.fr>
2021-11-18 15:18:06 +0100

    g froid

--- rank04_exam_microshell/microshell/test.c
+++ rank04_exam_microshell/microshell/test.c
@@ -1,8 +1,9 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+
 
 char **find_pipe(char **cmd)
 {
@@ -11,8 +12,43 @@ char **find_pipe(char **cmd)
 	int i = -1;
 	while (cmd[++i])
 		if (!strcmp(cmd[i], "|"))
-			return (&cmd[i+1]);
+			return (&cmd[i + 1]);
 	return (NULL);
+}
+
+int	error(char *str)
+{
+	int i = -1;
+	while (str[++i])
+		write(STDERR_FILENO, &str[i], 1);
+	return (1);
+}
+
+int fatal(char **ptr)
+{
+	free(ptr);
+	exit(error("error: fatal\n"));
+}
+
+int exe_cmd(char **cmd, char **env)
+{
+	pid_t pid;
+
+	if ((pid = fork()) < 0)
+		fatal(cmd);
+
+	if (!pid)
+	{
+		if (execve(cmd[0], cmd, env) < 0)
+		{
+			error("error exe: ");
+			error(cmd[0]);
+		//	free(cmd[0]);
+			exit(error("\n"));
+		}
+	}
+	waitpid(0, NULL, 0);
+	return (0);
 }
 
 int	size_char(char **cmd, char *str)
@@ -26,42 +62,7 @@ int	size_char(char **cmd, char *str)
 	return (i);
 }
 
-int	error(char *s)
-{
-	int i =-1;
-
-	while (s[++i])
-		write(STDERR_FILENO, &s[i], 1);
-	return (1);
-}
-
-int fatal(char **ptr)
-{
-	free(ptr);
-	exit(error("error: fatal\n"));
-}
-
-int	exe_cmd(char **cmd, char **env)
-{
-	pid_t pid;
-
-	if ((pid = fork()) < 0)
-		fatal(cmd);
-	if (!pid)
-	{
-		if (execve(cmd[0], cmd, env) < 0)
-		{
-			error("exec error: ");
-			error(cmd[0]);
-			free(cmd);
-			exit(error("\n"));
-		}
-	}
-	waitpid(0, NULL, 0);
-	return (0);
-}
-
-int	exe_child(char **cmd, char **tmp, int *fd_pipe, int fd_in, char **env)
+int	exe_child(char **cmd, char **tmp, char **env, int *fd_pipe, int fd_in)
 {
 	if (dup2(fd_in, STDIN_FILENO) < 0)
 		fatal(cmd);
@@ -79,7 +80,7 @@ int	exe_child(char **cmd, char **tmp, int *fd_pipe, int fd_in, char **env)
 	exit(0);
 }
 
-int	execute(char **cmd, char **env)
+int execute(char **cmd, char **env)
 {
 	if (find_pipe(cmd) == NULL)
 		return (exe_cmd(cmd, env));
@@ -91,16 +92,14 @@ int	execute(char **cmd, char **env)
 	pid_t pid;
 
 	if ((fd_in = dup(STDIN_FILENO)) < 0)
-		fatal(cmd);
-	
+		return (fatal(cmd));
+
 	while (tmp)
 	{
 		if (pipe(fd_pipe) < 0 || (pid = fork()) < 0)
 			fatal(cmd);
-
 		if (!pid)
-			exe_child(cmd, tmp, fd_pipe, fd_in, env);
-
+			exe_child(cmd, tmp, env, fd_pipe, fd_in);
 		else
 		{
 			if (dup2(fd_pipe[0], fd_in) < 0)
@@ -120,23 +119,21 @@ int	execute(char **cmd, char **env)
 	return (0);
 }
 
-char **add_cmd(char **av, int *i)
+char **add_cmd(char **cmd, int *i)
 {
-	int size = size_char(&av[*i], ";");
+	int size = size_char(&cmd[*i], ";");
 	if (!size)
 		return (NULL);
 	
 	char **tmp = NULL;
 	if (!(tmp = malloc(sizeof(*tmp) * (size + 1))))
 		fatal(NULL);
-	
+
 	int j = -1;
 	while (++j < size)
-		tmp[j] = av[j + *i];
+		tmp[j] = cmd[j + *i];
 	tmp[j] = NULL;
-
 	*i += size;
-
 	return (tmp);
 }
 
@@ -149,24 +146,23 @@ int size_cmd(char **cmd)
 	return (i);
 }
 
-int builtin_cd(char **cmd)
+int	builtin_cd(char **cmd)
 {
 	if (size_cmd(cmd) != 2)
-		return (error("bad args cd\n"));
+		return (error("cd arg wrooooong\n"));
 	if (chdir(cmd[1]) < 0)
 	{
-		error("cant go to ");
+		error("error chdir ");
 		error(cmd[1]);
 		error("\n");
 	}
 	return (0);
 }
 
-
 int	main(int ac, char **av, char **env)
 {
-	int i = 0;
 	char **cmd = NULL;
+	int i = 0;
 
 	while (++i < ac)
 	{
@@ -185,3 +181,4 @@ int	main(int ac, char **av, char **env)
 
 	return (0);
 }
+
