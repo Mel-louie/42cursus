449ac9c57da57ac3510c4be31c06d9475ab72c98
Mel DESFONTAINES <mdesfont@e2r7p6.clusters.42paris.fr>
2021-12-11 19:05:12 +0100

    adding containers

--- rank05_ft_containers/vector.hpp
+++ rank05_ft_containers/vector.hpp
@@ -6,17 +6,20 @@
 /*   By: mdesfont <mdesfont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 16:27:10 by mdesfont          #+#    #+#             */
-/*   Updated: 2021/12/11 16:31:18 by mdesfont         ###   ########.fr       */
+/*   Updated: 2021/12/11 18:56:28 by mdesfont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_CPP
 
+# include <iostream>
+
 namespace ft
 {
-    template <typename T>
-
+    template <typename T, class Alloc = std::allocator<T> >
+    class vector
+    {};
 };
 
 #endif
