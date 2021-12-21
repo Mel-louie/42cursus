02955fe57f2e1efeada64cf70229831b3379778d
Mel-louie <mdesfont@student.42.fr>
2021-12-21 18:30:03 +0100

    auto commit

--- rank05_ft_containers/test.sh
+++ rank05_ft_containers/test.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-clang++ -Wall -Wextra -Werror -std=c++98 -DTESTED_NAMESPACE=ft
+mkdir -p test/
+
+echo "Compile ft"
+clang++ -Wall -Wextra -Werror -std=c++98 -DTESTED_NAMESPACE=ft -o ft main.cpp
+
+echo "Compile stl"
+clang++ -Wall -Wextra -Werror -std=c++98 -DTESTED_NAMESPACE=std -o stl main.cpp
+
+./ft >> test/ft.txt
+./stl >> test/stl.txt
+
+DIFF=$(diff test/ft.txt test/stl.txt) 
+if [ "$DIFF" != "" ] 
+then
+    echo "The directory was modified"
+fi
+
