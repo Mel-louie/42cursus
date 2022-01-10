057cbf3f8aa63dcc68c7a9828cb4bd49427bd2cb
Mel-louie <mdesfont@student.42.fr>
2022-01-10 18:43:29 +0100

    map, don't like it

--- rank05_ft_containers/containers/rb_tree.hpp
+++ rank05_ft_containers/containers/rb_tree.hpp
@@ -66,6 +66,68 @@ namespace ft
 			else
 				return (search_tree_helper(node->right, key));
 		}
+
+		void fix_insert(node_ptr node)
+		{
+			// node parent is red, node too, it violated the RBT rules
+			node_ptr u; // uncle
+			while (node->parent->color == RED)
+			{
+				if (node->parent == node->parent->parent->right)
+				{
+					u = node->parent->parent->left;
+					if (u->color == RED)
+					{
+						// case 1
+						u->color = BLACK;
+						node->parent->color = BLACK;
+						node->parent->parent->color = RED;
+						node = node->parent->parent;
+					}
+					else
+					{
+						if (node == node->parent->left)
+						{
+							// case 2.2
+							node = node->parent;
+							right_rotate(node);
+						}
+						// case 2.1
+						node->parent->color = BLACK;
+						node->parent->parent->color = RED;
+						left_rotate(node->parent->parent);
+					}
+				}
+				else // same but miror
+				{
+					u = node->parent->parent->right;
+					if (u->color == RED)
+					{
+						// miror case 1
+						u->color = BLACK;
+						node->parent->color = BLACK;
+						node->parent->parent->color = RED;
+						node = node->parent->parent;
+					}
+					else
+					{
+						if (node == node->parent->right)
+						{
+							// mirror case 2.2
+							node = node->parent;
+							left_rotate(node);
+						}
+						// mirror case 2.1
+						node->parent->color = 0;
+						node->parent->parent->color = 1;
+						right_rotate(node->parent->parent);
+					}
+				}
+				if (node == root)
+					break ;
+			}
+			root->color = BLACK;
+		}
 		
 	public:
 		/*--------------------------------------------------------------*/
@@ -145,6 +207,102 @@ namespace ft
 				y = y->parent;
 			}
 			return (y);
+		}
+
+		// rotate left at node x
+		void left_rotate(node_ptr x)
+		{
+			node_ptr y = x->right;
+			x->right = y->left;
+
+			if (y->left != TNULL)
+				y->left->parent = x;
+
+			y->parent = x->parent;
+
+			if (x->parent == NULL)
+				this->root = y;
+			else if (x == x->parent->left)
+				x->parent->left = y;
+			else
+				x->parent->right = y;
+
+			y->left = x;
+			x->parent = y;
+		}
+
+		// rotate right at node x
+		void right_rotate(node_ptr x)
+		{
+			node_ptr y = x->left;
+			x->left = y->right;
+
+			if (y->right != TNULL)
+				y->right->parent = x;
+
+			y->parent = x->parent;
+
+			if (x->parent == NULL)
+				this->root = y;
+			else if (x == x->parent->right)
+				x->parent->right = y;
+			else 
+				x->parent->left = y;
+
+			y->right = x;
+			x->parent = y;
+		}
+
+		/*
+		*	To insert a new node in a RTB we need to:
+		*	- insert node using an ordinary BST insertion operation
+		*	- color the new node in red
+		*	- check if the insertion violated the RBT properties, if
+		*	it did, we fix it
+		*/
+		void insert(int key)
+		{
+			// ordinary BST insertion
+			node_ptr node = new Node;
+			node->parent = NULL;
+			node->_key = key;
+			node->left = TNULL;
+			node->right = TNULL;
+			node->color = RED;
+
+			node_ptr y = NULL;
+			node_ptr x = this->root;
+
+			while (x != TNULL)
+			{
+				// y saves x, to become the parent of x
+				y = x;
+				if (node->_key < x->_key)
+					x = x->left;
+				else
+					x = x->right;
+			}
+			node->parent = y; // since y is the parent of x
+			if (y == NULL) // x doesn't have parent, it's root
+				root = node;
+			else if (node->_key < y->_key)
+				y->left = node;
+			else
+				y->right = node;
+			
+			// if node is a root node
+			if (node->parent == NULL)
+			{
+				// change the color to black, a root is always black
+				node->color = BLACK;
+				return ;
+			}
+			// if the grandparent is nuul
+			if (node->parent->parent == NULL)
+				return ;
+
+			// fix the tree
+			fix_insert(node);
 		}
 	};
 }; // namespace ft
